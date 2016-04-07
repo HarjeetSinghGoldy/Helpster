3e042287fd283861e03b47940965b143200f0305
HarjeetSinghGoldy <harjeetsinghgoldy@gmail.com>
2016-04-07 16:18:05 +0530

    User Controller with logut action

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,10 +6,7 @@
  */
 
 module.exports = {
-  /**
-   * Check the provided email address and password, and if they
-   * match a real user in the database, sign in to Medool.
-   */
+
   login: function (req, res) {
 
     // Try to look up user using the provided email address
@@ -124,6 +121,29 @@ module.exports = {
           }
         });
       }
+    });
+  },
+  //Logout the user and wipe-out the session.me
+  logout: function (req, res) {
+
+    // Look up the user record from the database which is
+    // referenced by the id in the user session (req.session.me)
+    User.findOne(req.session.me, function foundUser(err, user) {
+      if (err)
+        return res.negotiate(err);
+
+      // If session refers to a user who no longer exists, still allow logout.
+      if (!user) {
+        sails.log.verbose('Session refers to a user who no longer exists.');
+        return res.backToHomePage();
+      }
+
+      // Wipe out the session (log out)
+      req.session.me = null;
+
+      // Either send a 200 OK or redirect to the home page
+      return res.backToHomePage();
+
     });
   }
 
